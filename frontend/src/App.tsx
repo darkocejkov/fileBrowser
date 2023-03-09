d52114b485e7f96d3bc0570c9a8abfa55eab3b82
darkocejkov <darkocheykov@gmail.com>
2023-03-08 21:18:39 -0500

    first real commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from "react";
+
+function App() {
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div className={'h-screen w-screen'}>
+
+    </div>
+  );
+}
+
+export default App;
